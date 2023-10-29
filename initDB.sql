e23a93ac99dbdd96c17ebe54946cebe5c32c59f0
Alexander <alexanderalmeida20@gmail.com>
2023-10-29 02:50:25 -0300

    Database

--- initDB.sql
+++ initDB.sql
@@ -1,13 +1,52 @@
-DROP USER IF EXISTS 'paintingUser'@'localhost';
-CREATE USER 'paintingUser'@'localhost' IDENTIFIED BY 'painting2539';
+-- MariaDB dump 10.19  Distrib 10.11.4-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: painting
+-- ------------------------------------------------------
+-- Server version	10.11.4-MariaDB-1~deb12u1
 
-GRANT ALL PRIVILEGES ON *.* TO 'paintingUser'@'localhost';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-FLUSH PRIVILEGES;
+--
+-- Table structure for table `colors`
+--
 
-DROP DATABASE `painting`;
-CREATE Database `painting`;
-USE `painting`;
+DROP TABLE IF EXISTS `colors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `colors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `command` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `colors`
+--
+
+LOCK TABLES `colors` WRITE;
+/*!40000 ALTER TABLE `colors` DISABLE KEYS */;
+INSERT INTO `colors` VALUES
+(1,'black','Negro','6'),
+(2,'crimson','Rojo','7'),
+(3,'cornflowerblue','Azul','8');
+/*!40000 ALTER TABLE `colors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `config`
+--
 
 DROP TABLE IF EXISTS `config`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -25,32 +64,10 @@ CREATE TABLE `config` (
 
 LOCK TABLES `config` WRITE;
 /*!40000 ALTER TABLE `config` DISABLE KEYS */;
-INSERT INTO `config` VALUES ('ip1','192.168.0.102'),('ip2','192.168.0.100:8080');
+INSERT INTO `config` VALUES
+('ip1','192.168.0.102'),
+('ip2','192.168.0.104:8080');
 /*!40000 ALTER TABLE `config` ENABLE KEYS */;
-UNLOCK TABLES;
-
-DROP TABLE IF EXISTS `colors`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `colors` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `command` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-);
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `config`
---
-
-LOCK TABLES `colors` WRITE;
-/*!40000 ALTER TABLE `colors` DISABLE KEYS */;
-INSERT INTO `colors` (`value`,`name`,`command`) VALUES ('black','Negro','6');
-INSERT INTO `colors` (`value`,`name`,`command`) VALUES ('crimson','Rojo','7');
-INSERT INTO `colors` (`value`,`name`,`command`) VALUES ('cornflowerblue','Azul','8');
-/*!40000 ALTER TABLE `colors` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -64,23 +81,27 @@ CREATE TABLE `connections` (
   `ip` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL DEFAULT '',
   `authorized` tinyint(1) NOT NULL DEFAULT 0,
-  `colorId` INT NOT NULL DEFAULT 1,
+  `colorId` int(11) NOT NULL DEFAULT 1,
   `colorTime` varchar(50) NOT NULL,
   `connectionTime` datetime NOT NULL,
-  
   PRIMARY KEY (`ip`),
-  FOREIGN KEY (`colorId`) REFERENCES `colors`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `colorId` (`colorId`),
+  CONSTRAINT `connections_ibfk_1` FOREIGN KEY (`colorId`) REFERENCES `colors` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 --
 -- Dumping data for table `connections`
 --
 
 LOCK TABLES `connections` WRITE;
 /*!40000 ALTER TABLE `connections` DISABLE KEYS */;
+INSERT INTO `connections` VALUES
+('192.168.0.100','clevo',1,1,'2023-10-29 05:43:25','2023-10-29 05:43:07'),
+('192.168.0.150','Carlos',1,1,'2023-10-29 04:58:03','2023-10-29 04:49:28'),
+('192.168.1.5','Pepe',1,3,'2023-10-29 02:24:08','2023-10-29 02:24:06');
 /*!40000 ALTER TABLE `connections` ENABLE KEYS */;
 UNLOCK TABLES;
-
 
 --
 -- Table structure for table `users`
@@ -102,7 +123,18 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('admin','2539');
+INSERT INTO `users` VALUES
+('admin','2539');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-29  2:49:33
